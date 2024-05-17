a12cd20ed3219d246d3988674208b20567690548
Lena Benoit <119876901+mahdi-salimi05@users.noreply.github.com>
2024-05-17 17:57:45 +0330

    ported fox_fire.mk from fox_mikona.mk
    fixed screen NOTCH problem

--- fox_fire.mk
+++ fox_fire.mk
@@ -1,1 +1,35 @@
+#
+#	This file is part of the OrangeFox Recovery Project
+# 	Copyright (C) 2023-2024 The OrangeFox Recovery Project
+#
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	any later version.
+#
+#	OrangeFox is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+# 	This software is released under GPL version 3 or any later version.
+#	See <http://www.gnu.org/licenses/>.
+#
+# 	Please maintain this if you use this script or any part of it
+#
 
+# screen settings
+OF_STATUS_INDENT_LEFT := 56
+OF_STATUS_INDENT_RIGHT := 48
+OF_HIDE_NOTCH := 1
+OF_CLOCK_POS := 1 
+
+# other stuff
+OF_IGNORE_LOGICAL_MOUNT_ERRORS := 1
+OF_USE_GREEN_LED := 0
+OF_QUICK_BACKUP_LIST := /boot;/data;
+OF_ENABLE_LPTOOLS := 1
+
+# ----- data format stuff -----
+# automatically wipe /metadata after data format
+OF_WIPE_METADATA_AFTER_DATAFORMAT := 1

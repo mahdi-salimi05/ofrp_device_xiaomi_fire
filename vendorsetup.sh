87a601a7d31f18d85bb7eb466869ac6f1fc36746
Lena Benoit <119876901+mahdi-salimi05@users.noreply.github.com>
2024-05-17 19:04:40 +0330

    Update vendorsetup.sh
    fixed some issues

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,79 +1,48 @@
-#
-#	This file is part of the OrangeFox Recovery Project
-# 	Copyright (C) 2021-2024 The OrangeFox Recovery Project
-#
-#	OrangeFox is free software: you can redistribute it and/or modify
-#	it under the terms of the GNU General Public License as published by
-#	the Free Software Foundation, either version 3 of the License, or
-#	any later version.
-#
-#	OrangeFox is distributed in the hope that it will be useful,
-#	but WITHOUT ANY WARRANTY; without even the implied warranty of
-#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#	GNU General Public License for more details.
-#
-# 	This software is released under GPL version 3 or any later version.
-#	See <http://www.gnu.org/licenses/>.
-#
-# 	Please maintain this if you use this script or any part of it
-#
+export FOX_USE_SPECIFIC_MAGISK_ZIP=~/device/xiaomi/fire/Magisk/Magisk-v27.0.zip
+export OF_FL_PATH1="/sys/class/leds/torch-light0"
 
-#set -o xtrace
-FDEVICE="fire"
-THIS_DEVICE=${BASH_ARGV[2]}
+ # Some about us
+export FOX_VERSION="R12.1"
+export OF_MAINTAINER="mahdi-salimi05"
 
-fox_get_target_device() {
-local chkdev=$(echo "$BASH_SOURCE" | grep -w \"$FDEVICE\")
-   if [ -n "$chkdev" ]; then 
-      FOX_BUILD_DEVICE="$FDEVICE"
-   else
-      chkdev=$(set | grep BASH_ARGV | grep -w \"$FDEVICE\")
-      [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
-   fi
-}
+# Build environment stuffs
+#export FOX_BUILD_DEVICE="Redmi12"
+export ALLOW_MISSING_DEPENDENCIES=true
+export BUILD_USERNAME="nobody"
+export BUILD_HOSTNAME="android-build"
 
-if [ "$THIS_DEVICE" = "alioth" -o "$THIS_DEVICE" = "munch" ]; then
-	FDEVICE="$THIS_DEVICE"
-	[ -z "$FOX_BUILD_DEVICE" ] && FOX_BUILD_DEVICE="$THIS_DEVICE"
-fi
+# Use magisk boot for patching
+export OF_USE_MAGISKBOOT=1
+export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
+export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
 
-if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" -a -z "$FDEVICE" ]; then
-   fox_get_target_device
-fi
+# We have a/b partitions
+export FOX_AB_DEVICE=1
+export FOX_VIRTUAL_AB_DEVICE=1
 
-if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
+# Screen specifications
+export OF_STATUS_INDENT_LEFT=48
+export OF_STATUS_INDENT_RIGHT=48
+export OF_ALLOW_DISABLE_NAVBAR=0
+export OF_CLOCK_POS=1
+export OF_SCREEN_H=2460
+export OF_STATUS_H=120
 
-	if [ -z "$THIS_DEVICE" ]; then
-		echo "ERROR! This script requires bash. Run '/bin/bash' and build again."
-		exit 1
-	fi
+# Device stuff
+export OF_KEEP_FORCED_ENCRYPTION=1
+export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
 
-	export FOX_USE_SPECIFIC_MAGISK_ZIP=~/device/xiaomi/fire/Magisk/Magisk-v27.0.zip
-        export FOX_VANILLA_BUILD=1
-    	export FOX_ENABLE_APP_MANAGER=1
-	export FOX_VIRTUAL_AB_DEVICE=1
-	export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
-	export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
-	export FOX_USE_BASH_SHELL=1
-	export FOX_ASH_IS_BASH=1
-	export FOX_USE_TAR_BINARY=1
-	export FOX_USE_SED_BINARY=1
-	export FOX_USE_XZ_UTILS=1
-	export FOX_USE_NANO_EDITOR=1
-    	export FOX_DELETE_AROMAFM=1
-    	export FOX_BUGGED_AOSP_ARB_WORKAROUND="1616300800"; # Sun 21 Mar 04:26:40 GMT 2021
-    	export TARGET_DEVICE_ALT="aliothin"
+# OTA for custom ROMs
+export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
 
-	# instruct magiskboot v24+ to always patch the vbmeta header when patching the recovery/boot image; do *not* remove!
-        export FOX_PATCH_VBMETA_FLAG="1"
+# Quick Backup list
+export OF_QUICK_BACKUP_LIST="/boot;/data;/super;"
 
-	# vendor_boot-as-recovery
-	if [ "$FOX_VENDOR_BOOT_RECOVERY" = "1" ]; then
-	   export FOX_VARIANT="vBaR"
-	fi
-else
-	if [ -z "$FOX_BUILD_DEVICE" -a -z "$BASH_SOURCE" ]; then
-		echo "I: This script requires bash. Not processing the $FDEVICE $(basename $0)"
-	fi
-fi
-#
+# Dont install AROMAFM
+export FOX_DELETE_AROMAFM=1
+
+# Add some extras
+export FOX_USE_BASH_SHELL=1
+export FOX_USE_NANO_EDITOR=1
+export OF_NO_SPLASH_CHANGE=1
+export FOX_INSTALLER_DISABLE_AUTOREBOOT=1
